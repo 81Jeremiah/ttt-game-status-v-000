ecc222c525eb8093af0f2f45fd5a144873a0ad53
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-04 23:41:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,9 +38,9 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) == ["X","X","X"]
+  if won?(board) == ["X,X,X"]
   return "X"
-elsif won?(board) == ["O","O","O"]
+elsif won?(board) == ["O,O,O"]
   return "O"
 else false
   end
